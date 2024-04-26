311b1714de89fcba55627b6622d5442a6563b33d
jeonghun.lee <jeonghun.lee@twolinecode.com>
2024-04-26 12:25:15 +0900

    first commit

---
+++ chapter4/4.9_Mixins.dart
@@ -0,0 +1,30 @@
+mixin Strong {
+  final double strengthLevel = 1500.99;
+}
+
+mixin QuickRunner {
+  void runQuick() {
+    print("ruuuuuuuun!");
+  }
+}
+
+mixin Tall {
+  final double height = 1.99;
+}
+
+enum Team { blue, red }
+
+class Player with Strong, QuickRunner, Tall {
+  final Team team;
+
+  Player({required this.team});
+}
+
+class Horse with Strong, QuickRunner {}
+
+class Kid with QuickRunner {}
+
+void main() {
+  var player = Player(team: Team.red);
+  player.runQuick();
+}
---
+++ main.dart
@@ -0,0 +1,11 @@
+String sayHello(
+  String name,
+  int age,
+  [String? country = 'cuba']
+) {
+  return 'Hello $name, you are $age, and you come from $country';
+}
+
+void main() {
+  print(sayHello('nico', 20));
+}
